796bc0c0fc067d5b6abfd88de7c223b53a38bf81
Talha Abbas <talhaabbas556989@gmail.com>
2021-12-22 01:25:38 +0500

    before starting forms

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 
@@ -11,9 +12,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <React.StrictMode>
+          <Header />
           <Switch>
             <Route exact path="/" component={Homepage} />
-            <Route path="/shop/hats" component={ShopPage} />
+            <Route path="/shop" component={ShopPage} />
           </Switch>
         </React.StrictMode>
       </BrowserRouter>
