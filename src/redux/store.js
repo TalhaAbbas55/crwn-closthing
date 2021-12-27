38d85d0c54abec0bb46154e53bfce29b33735764
Talha Abbas <talhaabbas556989@gmail.com>
2021-12-27 21:35:08 +0500

    fixing the build packs

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
